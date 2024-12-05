a29b3f53fdccb925e20f8e9d8e6b85fc9385df21
klim1103 <klim999313@gmail.com>
2024-12-05 15:28:51 +0700

    Add files via upload

---
+++ while.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+numbers= len(my_list)
+index = 0
+
+while index < numbers:
+    if my_list [index] == 0:
+        index = index + 1
+        continue
+    elif my_list [index] > 0:
+        print(my_list[index])
+        index = index + 1
+    elif my_list [index] < 0:
+        break
